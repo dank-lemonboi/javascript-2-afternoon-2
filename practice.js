5c497adc11f30b66f108fa592e938bc096e146c0
dank-lemonboi <nictayt@gmail.com>
2018-01-30 14:56:28 -0700

    afternoon problems

--- practice.js
+++ practice.js
@@ -17,8 +17,7 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+var first = (arr) => arr[0];
 
 
 ////////// PROBLEM 2 //////////
@@ -32,8 +31,7 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+var last = (arr) => arr[arr.length-1];
 
 
 ////////// PROBLEM 3 //////////
@@ -47,8 +45,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+var looper = (family) => {
+  for(let i = 0;i < family.length;i++){
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,8 +63,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
-
+reversedLooper = (letters) => {
+  for(let i = letters.length - 1; i >= 0; i--){
+     alert(letters[i]);
+  }
+}
+ 
 
 
 ////////// PROBLEM 5 //////////
@@ -77,9 +82,8 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return only the even numbers from the array.
 */
 
-//Code Here
-
-
+evenFinder = (nums) => nums.filter((item) => item % 2 === 0);
+  
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
 
